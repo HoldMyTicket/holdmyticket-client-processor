e1899f6cd571a9c1220a41b73901d8aa13b53b12
Joseph Perez <josephperez717@gmail.com>
2019-12-04 16:05:45 -0700

    getting our first unit test to pass :tada:

--- src/__tests__/hmt-client-processor.js
+++ src/__tests__/hmt-client-processor.js
@@ -1,3 +1,18 @@
-import hmtClientProcessor from '../hmt-client-processor';
+import hmt_client_processor from '../hmt-client-processor';
 
-console.log(hmtClientProcessor);
+const hmt_client_processor_settings = {
+  api_url : 'http://holdmyticket.loc/api/',
+  env : 'dev',
+  api_url_suffix : ''
+}
+
+test('_format_phone_number removes all special characters', () => {
+  const cc_processor = new hmt_client_processor(hmt_client_processor_settings);
+
+  const expected_phone_number = '5055555555';
+  const phone_number = '(505)-555-5555';
+
+  const formatted_phone_number = cc_processor._format_phone_number(phone_number);
+
+  expect(formatted_phone_number).toBe(expected_phone_number);
+})

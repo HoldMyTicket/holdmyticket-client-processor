f4f87c8a958d92ba5d7b947e108b6dc58d9c01fd
Thomas Stevens <thomaalanstevens@gmail.com>
2023-01-18 12:59:32 -0800

    Better way to check for and add to error msg.

--- src/hmt-client-processor.js
+++ src/hmt-client-processor.js
@@ -327,28 +327,29 @@ var hmt_client_processor = function(settings){
         var CVVResponseDescription = res.issuerResponseDetails.cvvResponseDescription || "";
         var responseError = res.responseCode || "";
 
-        if (avsResponseCode != "D" && avsResponseCode != "E" && avsResponseCode != "F" && avsResponseCode != "J" && avsResponseCode != "K" && avsResponseCode != "M" && avsResponseCode != "X" && avsResponseCode != "Y" && avsResponseCode != "0" && msg == "") {
+        var goodAVSCodes = ["D","E","F","J","K","M","X","Y","0"];
+
+        if (!goodAVSCodes.includes(avsResponseCode)) {
           msg = "<br/>Card Declined: " + this.check_fullsteam_codes(AVS_response_codes, avsResponseCode, msg)
-          console.log("Card Declined AVS Error: " + this.check_fullsteam_codes(AVS_response_codes, avsResponseCode, msg));
         }
-        if (CVVResponseCode != "M" && msg == "") {
+
+        else if (CVVResponseCode != "M" && msg == "") {
           msg = "<br/>Card Declined: Card CVV Security Code Issue" 
-          console.log("Card Declined CVV Error: " + this.check_fullsteam_codes(CVV_response_codes, CVVResponseCode, msg));
-        } else {
-          if (this.errors_processing.length > 0 && issuerResponseCode == "00")
-            // we already have processing error, and there isn't a issuer error, so return...
-            return false;
-          if (msg == "" && (!issuerResponseCode || issuerResponseCode == "00"))
-            msg = "CPE2: Missing error code";
-          if (msg == "") msg = "CPE3: Unknown issuer error";
+        // } else {
+        //   if (this.errors_processing.length > 0 && issuerResponseCode == "00")
+        //     // we already have processing error, and there isn't a issuer error, so return...
+        //     return false;
+        //   if (msg == "" && (!issuerResponseCode || issuerResponseCode == "00"))
+        //     msg = "CPE2: Missing error code";
+        //   if (msg == "") msg = "CPE3: Unknown issuer error";
         }
-        if(issuerResponseCode && msg == ""){
+
+
+        else if(issuerResponseCode){
           msg = "<br/>Card Declined: An error has occurred, please check that all the information entered is correct and resubmit. If this issue persists please contact HoldMyTicket."
-          console.log("Card Declined Issuer Error: " + this.check_fullsteam_codes(error_issuer_response_codes, issuerResponseCode, msg));
         }
-        if(responseError && msg == ""){
+        else if(responseError){
           msg = "<br/>Card Declined: An error has occurred, please check that all the information entered is correct and resubmit. If this issue persists please contact HoldMyTicket."
-          console.log("Card Declined Response Error: " + this.check_fullsteam_codes(responseCodes, responseError, msg))
         }
       }
 
